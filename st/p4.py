af9845a092e2f8189768c1abbe35655d006d2bd6
SHANms227 <zuzuedits3012@gmail.com>
2026-02-25 13:54:20 +0530

    Delete st directory

--- st/p4.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver 
-driver = webdriver.Chrome() 
-driver.get("https://example.com") 
-driver.save_screenshot("screenshot.png") 
-print("Screenshot saved successfully") 
-driver.quit() 
